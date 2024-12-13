4c685e9716de1f75a24a33feeb0ae9951f257c06
Kamil Budzik <mixboksu@gmail.com>
2024-12-13 12:09:51 +0100

    Implement infinite loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,17 +6,20 @@ import (
 	"os"
 )
 
+func handleCommand() {
+	fmt.Fprint(os.Stdout, "$ ")
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
+}
+
 func main() {
 
 	for {
-
-		fmt.Fprint(os.Stdout, "$ ")
-
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
-		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		handleCommand()
 	}
 }
